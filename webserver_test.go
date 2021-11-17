b5f65d5397359c6bf39d032d4eff037c3e10c361
Yizhou Shan <syzwhat@gmail.com>
2021-11-17 12:26:13 -0800

    Prepare webserver test

---
+++ webserver_test.go
@@ -0,0 +1,20 @@
+// This file has tests for the web interface requests
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestHttpKVPut(t *testing.T) {
+}
+
+func TestHttpKVSet(t *testing.T) {
+}
+
+func TestHttpKVDump(t *testing.T) {
+}
